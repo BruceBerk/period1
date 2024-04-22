d8dc2bd5965630f9e883411731854aaecea3793d
Bruce Berkowicz <berknj@Bruces-MacBook-Pro.local>
2024-04-22 12:04:42 -0400

    break into functions and sample asking user what to do

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
-fn main() {
-    println!("Loading period txns list");
+use std::io::stdin;
 
+fn load_txns() {
+    println!("Loading period txns list");
+}
+
+fn process_txns() {
     println!("Processing period item list");
 
+    let mut user_ans = String::new();
+    println!("What do you want to do? (a)dd, (s)kip or (i)gnore?");
+    stdin()
+        .read_line(&mut user_ans)
+        .expect("Failed to read line");
+}
+
+fn write_txns() {
     println!("Writing revised period items list");
+}
+
+fn main() {
+    load_txns();
+
+    process_txns();
+
+    write_txns();
 }
